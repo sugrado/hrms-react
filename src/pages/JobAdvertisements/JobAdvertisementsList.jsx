4c6c83f541542db515a768a3634ae0ae84166e68
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2021-07-10 21:39:43 +0300

    Search/filter through data
    - Filter data with text

--- src/pages/JobAdvertisements/JobAdvertisementsList.jsx
+++ src/pages/JobAdvertisements/JobAdvertisementsList.jsx
@@ -1,22 +1,47 @@
 import React from "react";
-import { Card, Image } from "semantic-ui-react";
+import { Card, Image, Form, Label } from "semantic-ui-react";
 import { useEffect, useState } from "react";
 import JobAdvertisementService from "../../services/JobAdvertisementService";
 
 export default function JobAdvertisementsList() {
-  const [jobAdvertisements, setJobAdvertisements] = useState([]);
+  const [allData, setAllData] = useState([]);
+  const [filteredData, setFilteredData] = useState(allData);
 
   useEffect(() => {
     let jobAdvertisementService = new JobAdvertisementService();
     jobAdvertisementService.getApprovedAdvertisements().then((result) => {
-      setJobAdvertisements(result.data.data);
+      setAllData(result.data.data);
+      setFilteredData(result.data.data);
     });
   }, []);
 
+  const handleSearch = (event) => {
+    let value = event.target.value.toLowerCase();
+    let result = [];
+    result = allData.filter((data) => {
+      return (
+        data.jobPosition.name.toLowerCase().search(value) !== -1 ||
+        data.employmentType.name.toLowerCase().search(value) !== -1 ||
+        data.employer.companyName.toLowerCase().search(value) !== -1
+      );
+    });
+    setFilteredData(result);
+  };
+
   return (
     <div>
+      <Form>
+        <label>Search Text:</label>
+        <Form.Field inline>
+          <input
+            style={{ marginBottom: "20px" }}
+            type="text"
+            onChange={(event) => handleSearch(event)}
+          />
+        </Form.Field>
+      </Form>
       <Card.Group>
-        {jobAdvertisements.map((advert) => (
+        {filteredData.map((advert) => (
           <Card
             key={advert.id}
             fluid
